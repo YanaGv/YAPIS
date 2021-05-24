c8c858b890aaed29b6c749e6590288182e0ab5d9
YanaGv <gv.yanina@gmail.com>
2021-05-24 15:41:30 +0300

    add toString method to ParamNode

--- src/main/java/Node/ParamNode.java
+++ src/main/java/Node/ParamNode.java
@@ -1,10 +1,21 @@
 package Node;
 
-public class ParamNode extends Node{
+public class ParamNode extends Node {
     public String id;
     public String type;
-    public ParamNode (String s){
+
+    public ParamNode(String s) {
         this.type = s.split(" ")[0];
         this.id = s.split(" ")[1];
+    }
+
+    public String toString() {
+        String varType = type.substring(1, type.length() - 1);
+        if (varType.equals("element")) {
+            varType = "int";
+        } else if (varType.equals("list")) {
+            varType = "int[]";
+        }
+        return varType + " " + id;
     }
 }
