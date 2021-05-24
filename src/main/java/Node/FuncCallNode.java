19e2bc9da17a142deba0bfce86efa818b5d53b45
YanaGv <gv.yanina@gmail.com>
2021-05-24 15:40:26 +0300

    add toString method to FuncCallNode

--- src/main/java/Node/FuncCallNode.java
+++ src/main/java/Node/FuncCallNode.java
@@ -6,4 +6,17 @@ import java.util.List;
 public class FuncCallNode extends StatementNode {
     public String id;
     public List<MathExpressionNode> params = new ArrayList<>();
+
+    public String toString() {
+        String func = id + "(";
+        for (MathExpressionNode exp : params) {
+            func += exp.toString() + ",";
+        }
+        try {
+            func = func.substring(0, func.lastIndexOf(","));
+        } catch (Exception ex) {
+        }
+        func += ");";
+        return func;
+    }
 }
