d916dc5fcd95e6151b7bd74b43ce9b50c2f8c784
YanaGv <gv.yanina@gmail.com>
2021-05-24 15:41:08 +0300

    add toString method to LandingExpressionNode

--- src/main/java/Node/LandingExpressionNode.java
+++ src/main/java/Node/LandingExpressionNode.java
@@ -4,4 +4,8 @@ public class LandingExpressionNode extends StatementNode {
     public String id;
     public String op;
     public MathExpressionNode mathExpression;
+
+    public String toString() {
+        return id +" " + op + "=" + mathExpression.toString() + ";";
+    }
 }
